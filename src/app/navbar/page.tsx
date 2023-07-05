3d4fd9039b09302d6667d5f4cfd2aa15a6ff31d3
alvinChristianto <alvinchristianto93@gmail.com>
2023-07-05 23:19:24 +0700

    add components

---
+++ src/app/navbar/page.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Navbar (props:any) {
+  return (
+    <div>
+      this is navbar
+    </div>
+  )
+};
+
+export default Navbar;
---
+++ src/app/profile1/page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function blog(props: any) {
+  return (
+    <>
+     <div className="container">
+      <div className="flex flex-wrap">
+        <div className="w-full self-center px-4">
+          <h1>Hallo, Saya Alvin Christianto </h1>
+          <h2>IT Enthusiast | Business Analyst | Frontend | Blockchains</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur incidunt assumenda dolores odit, officia commodi.</p>
+          
+        </div>
+      </div>
+     </div>
+    </>
+  );
+}
+
+export default blog;
